286c32fe985ee774ffb450f4e0ed93d510d2fe0c
SamyuktaKarthikeyan <727721eucs129@skcet.ac.in>
2024-03-01 12:30:44 +0530

    Chenges to base file

---
+++ Supply-Chain-Management-starter-file-main/pages/_app.js
@@ -0,0 +1,20 @@
+import "../styles/globals.css";
+
+//INTERNAL IMPORT
+
+import { TrackingProvider } from "../Context/Tracking";
+import { NavBar, Footer } from "../Components";
+
+
+export default function App({ Component, pageProps }) {
+  
+  return(
+  <>
+  <TrackingProvider>
+  <NavBar/>
+   <Component {...pageProps} />
+   </TrackingProvider>
+   <Footer/>
+   </>
+  );
+}
